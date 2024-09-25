7f76d84d853b879a922f01ee96a6a9b381b99786
LucaDeeken <168202686+LucaDeeken@users.noreply.github.com>
2024-09-25 10:04:40 +0200

    create library-array and add dummy-books

---
+++ script.js
@@ -0,0 +1,5 @@
+const myLibrary = [ 
+    {title: "The Outsider", author: "Albert Camus", sites: 159, read: true},
+    {title: "The Brothers Karamasow", author: "Fjodor Dostojewski", sites: 1064, read: true},
+    {title: "Buddenbrooks", author: "Thomas Mann", sites: 768, read: false},
+  ];
