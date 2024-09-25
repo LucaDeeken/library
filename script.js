f0931ae9ad18d864715d4f696d00a16947b6d105
LucaDeeken <168202686+LucaDeeken@users.noreply.github.com>
2024-09-25 14:31:30 +0200

    add loop that shows library on cards

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ const myLibrary = [
     {title: "The Brothers Karamasow", author: "Fjodor Dostojewski", sites: 1064, read: true},
     {title: "Buddenbrooks", author: "Thomas Mann", sites: 768, read: false},
   ];
+  
+//project library on cards 
+const cardContent = document.getElementsByClassName("content");
+let j = 0;
+
+for (let i=0; i<myLibrary.length; i++) {
+
+    const books = myLibrary[i];
+
+    for (let key in books) {
+        cardContent[j].textContent = `${books[key]}`; j++ 
+    }    
+}
+
+  function Book() {
+    // the constructor...
+  }
+  
+  function addBookToLibrary() {
+    // do stuff here
+  }
